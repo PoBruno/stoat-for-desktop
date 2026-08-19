b6b90c4304706f480fd98cb7e3ed8c4cfe0c6b5d
Jacob Schlecht <Dadadah@users.noreply.github.com>
2026-08-19 12:25:18 -0600

    fix: Add temp appimage workflow, fix some ci/cd bugs (#285)
    * fix: Add temp appimage workflow, try fixing icon
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>
    
    * fix: Remove electron-vite from tsconfig
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>
    
    * fix: run the workflow when labeled
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>
    
    ---------
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- .github/build/appimage/make-appimage.sh
+++ .github/build/appimage/make-appimage.sh
@@ -7,14 +7,13 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=https://raw.githubusercontent.com/stoatchat/assets/f106946659af67ad4f008588ac51570029b2fd47/desktop/icon.png
-export DESKTOP=https://raw.githubusercontent.com/stoatchat/for-desktop/refs/heads/main/chat.stoat.StoatDesktop.desktop
-export DEPLOY_PIPEWIRE=1
+
+cp -v ./assets/desktop/icon.svg ./AppDir/.DirIcon
+cp -v ./assets/desktop/icon.svg ./AppDir/
+cp -v ./chat.stoat.StoatDesktop.desktop ./AppDir/
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
